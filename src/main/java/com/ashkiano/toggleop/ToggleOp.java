eeed72b2d8d1639c05cc0ba0f60c2122493e8c2c
AshKiano <ash.kiano@gmail.com>
2023-07-03 21:00:27 +0200

    version 1.3

--- src/main/java/com/ashkiano/toggleop/ToggleOp.java
+++ src/main/java/com/ashkiano/toggleop/ToggleOp.java
@@ -2,12 +2,31 @@ package com.ashkiano.toggleop;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public class ToggleOp extends JavaPlugin {
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        this.getCommand("toggleop").setExecutor(new ToggleOpCommand(this));
+
+        // Create a directory named "languages" in the plugin's data folder
+        File languageFolder = new File(getDataFolder(), "languages");
+        if (!languageFolder.exists()) {
+            languageFolder.mkdirs();
+        }
+
+        // Check if language files exist, if not, save them from the resources
+        String[] languages = {"en", "cs"};
+        for (String lang : languages) {
+            File langFile = new File(languageFolder, lang + ".yml");
+            if (!langFile.exists()) {
+                this.saveResource("languages/" + lang + ".yml", false);
+            }
+        }
+
+        String languageCode = getConfig().getString("language", "en"); // default language is English
+        this.getCommand("toggleop").setExecutor(new ToggleOpCommand(this, languageCode));
         Metrics metrics = new Metrics(this, 18886);
     }
 }
--- src/main/java/com/ashkiano/toggleop/ToggleOpCommand.java
+++ src/main/java/com/ashkiano/toggleop/ToggleOpCommand.java
@@ -3,45 +3,47 @@ package com.ashkiano.toggleop;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
-//TODO přidat hlášky do configu
+import java.io.File;
+
 //TODO přidat překlady
 //TODO přidat možnosti časů ve kterých si může dát OP, mimo tento interval se OP odebere
+
 // This class handles the /toggleop command.
 public class ToggleOpCommand implements CommandExecutor {
 
     private ToggleOp plugin;
+    private FileConfiguration langConfig;
     private String noPermissionMessage;
     private String nonPlayerMessage;
     private String revokeOpMessage;
     private String grantOpMessage;
 
-    // This constructor takes a reference to the plugin instance and reads
-    // the messages from the config.
-    public ToggleOpCommand(ToggleOp plugin) {
+    public ToggleOpCommand(ToggleOp plugin, String languageCode) {
         this.plugin = plugin;
-        // Load the messages from the config
-        this.noPermissionMessage = plugin.getConfig().getString("messages.noPermission", "You do not have permission to use this command!");
-        this.nonPlayerMessage = plugin.getConfig().getString("messages.nonPlayer", "Only players can use this command!");
-        this.revokeOpMessage = plugin.getConfig().getString("messages.revokeOp", "OP status has been revoked.");
-        this.grantOpMessage = plugin.getConfig().getString("messages.grantOp", "OP status has been granted.");
+        // Load the messages from the language specific config
+        File langFile = new File(plugin.getDataFolder() + "/languages", languageCode + ".yml");
+        this.langConfig = YamlConfiguration.loadConfiguration(langFile);
+
+        this.noPermissionMessage = langConfig.getString("messages.noPermission", "You do not have permission to use this command!");
+        this.nonPlayerMessage = langConfig.getString("messages.nonPlayer", "Only players can use this command!");
+        this.revokeOpMessage = langConfig.getString("messages.revokeOp", "OP status has been revoked.");
+        this.grantOpMessage = langConfig.getString("messages.grantOp", "OP status has been granted.");
     }
 
-    // This method handles the /toggleop command.
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // Check if the command sender is a player
         if (!(sender instanceof Player)) {
             sender.sendMessage(nonPlayerMessage);
             return true;
         }
 
         Player player = (Player) sender;
-        // Check if the player has permission to use the /toggleop command
         String permission = plugin.getConfig().getString("permission", "toggleop");
         if (player.hasPermission(permission)) {
-            // Check if the player has OP status
             if (player.isOp()) {
                 player.setOp(false);
                 player.sendMessage(revokeOpMessage);
